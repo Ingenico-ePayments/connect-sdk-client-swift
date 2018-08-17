0891e338669c7ab3b193790b571715ce488104e8
Ingenico ePayments <jenkins@isaac.nl>
2018-08-17 15:46:57 +0200

    Release 2.4.0.

--- IngenicoConnectKit/Util.swift
+++ IngenicoConnectKit/Util.swift
@@ -38,7 +38,7 @@ public class Util {
     public init() {
         metaInfo = [
             "platformIdentifier": platformIdentifier,
-            "sdkIdentifier": "SwiftClientSDK/v2.3.0",
+            "sdkIdentifier": "SwiftClientSDK/v2.4.0",
             "sdkCreator": "Ingenico",
             "screenSize": screenSize,
             "deviceBrand": "Apple",
