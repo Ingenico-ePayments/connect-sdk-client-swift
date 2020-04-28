baee45013c99073119b7841f9c0e6a743db71506
Ingenico ePayments <github@epay.ingenico.com>
2020-04-28 10:36:10 +0200

    Release 4.1.0.

--- IngenicoConnectKit/Util.swift
+++ IngenicoConnectKit/Util.swift
@@ -38,7 +38,7 @@ public class Util {
     public init() {
         metaInfo = [
             "platformIdentifier": platformIdentifier,
-            "sdkIdentifier": "SwiftClientSDK/v4.0.0",
+            "sdkIdentifier": "SwiftClientSDK/v4.1.0",
             "sdkCreator": "Ingenico",
             "screenSize": screenSize,
             "deviceBrand": "Apple",
